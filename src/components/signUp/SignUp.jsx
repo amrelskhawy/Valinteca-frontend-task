7e2760653f045d0937ac167408b69875bcc5bc21
Amr Elskhawy <amrelskhawy@gmail.com>
2022-12-08 07:08:20 +0200

    Finish fetching and Sending Data to the Server and GET 200 Respone

--- src/components/signUp/SignUp.jsx
+++ src/components/signUp/SignUp.jsx
@@ -1,21 +1,90 @@
 import React, { useState } from 'react'
 import Input from '../input/Input'
+import axios from 'axios'
 import './SignUp.scss'
-
 import { inputs } from '../../inputs'
+
+
 const SignUp = () => {
+	const [error, setError] = useState("")
 	const [formData, setFormData] = useState({
 		username: '',
 		email: '',
 		password: '',
 		password_confirmation: ''
 	})
-	const handleChange = (e) => { 
-		const {name,value} = e.target
-		setFormData((prev)=> (
-			{...prev,[name]: value}
-		))
-		
+	const handleChange = (e) => {
+		const { name, value } = e.target
+		setError("")
+		setFormData((prev) => ({ ...prev, [name]: value }))
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		function isValidEmail(email) {
+			return /\S+@\S+\.\S+/.test(email);
+		}
+
+		async function postData(url = '', data = {}) {
+			// Default options are marked with *
+			const response = await fetch(url, {
+				method: 'POST', // *GET, POST, PUT, DELETE, etc.
+				headers: {
+					'Content-Type': 'application/json'
+					// 'Content-Type': 'application/x-www-form-urlencoded',
+				},
+				
+				body: JSON.stringify(data) // body data type must match "Content-Type" header
+			});
+			return response // parses JSON response into native JavaScript objects
+		}
+
+		function isValidUserName(username) {
+			let isValid = false
+			if (
+				username.substr(0, 1) != NaN &&
+				username.substr(-1, 1) != NaN
+				&& (username.length >= 5 && username.length <= 15)) {
+				isValid = true
+			}
+			return isValid
+		}
+
+
+		// Validating Username Input 
+		const { username, password, password_confirmation, email } = formData
+
+		if (!isValidUserName(username)) {
+			setError("Username doesn't meet out requirments")
+
+		} else if (!isValidEmail(email)) {
+			setError("It's Not a Valid Email !")
+		}
+		else if (!password && !password_confirmation) {
+			setError("You Should Enter The Password")
+		} else if (password !== password_confirmation) {
+			setError("Passwords Should be Matched !!")
+		} else {
+
+			const response = {}; 
+			postData('https://goldblv.com/api/hiring/tasks/register', formData)
+				.then((data) => {
+					console.log(data)
+					if ( data.statusText === "OK" &&
+					data.status===200 ) {
+						window.navigator('/authenticate')
+						
+					}
+				});
+
+				
+
+		}
+
+
+
+
+
 	}
 	return (
 		<div className='sign-up-container'>
@@ -27,14 +96,16 @@ const SignUp = () => {
 						Go ahead and sign up, let everyone know how awesome you are!
 					</p>
 				</div>
-				<form>
+				<form method='post' onSubmit={handleSubmit}>
 					{
 						inputs.map((input, index) => (
 							<Input onChange={handleChange} value={formData[input.name]} key={index} {...input} />
 						))
 					}
+					{error && <div className='err'>{error}</div>}
+					<button type='submit'>Create Account</button>
 				</form>
-				<button onClick={()=> console.log(formData)} type='submit'>Create Account</button>
+
 			</div>
 		</div>
 	)
